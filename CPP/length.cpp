4daaefee14f02826caccc9a5b0b252d71c07addb
Jino Bae <bae_alex@naver.com>
2019-04-05 21:34:50 +0900

    [cpp] length

--- CPP/length.cpp
+++ CPP/length.cpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <iostream>
 
+// TODO :
+// fix - Trash Value of Length < 5 Array
+//     - memset(ARR,0,sizeof(ARR));
+// fix - 0 == NULL issue
+
 template <typename T>
 int Jlength(T *len = nullptr)
 {
@@ -10,15 +15,15 @@ int Jlength(T *len = nullptr)
 	while(1) {
 		size += sizeof(*(len + count));
 		count++;
-		if(*(len + count + 1) == NULL) {
+		if(*(len + count) == NULL) {
 			break;
 		}
 	}
-	return size/sizeof(T) + 1;
+	return size/sizeof(T);
 }
 
 /*
- * int x = {10,20,30,40,50};
+ * int x = {10,20,30,40,50,60};
  * Jlength(x)
  * 
  * char y = "BaeJino";
