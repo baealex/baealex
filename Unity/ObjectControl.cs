e75fade2ed96b490574f743a923c5ee673769712
baealex <im@baejino.com>
2019-05-15 01:24:06 +0900

    update unity record

--- Unity/ObjectControl.cs
+++ Unity/ObjectControl.cs
@@ -20,6 +20,8 @@ public class Controller : MonoBehaviour
         this.cube_yellow = GameObject.Find("Cube_Yellow");
     }
 
+    // Auto Moving
+    /*
     void Update()
     {
         this.cube_red.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
@@ -27,4 +29,29 @@ public class Controller : MonoBehaviour
         this.cube_green.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
         this.cube_yellow.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
     }
+    */
+
+    // Keyboard Input
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            this.cube_red.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime * (-1));
+        }
+        
+        /* // Just Once
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            this.cube_red.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime * (-1));
+        }
+        */
+    }
+
+    // Postion
+    /*
+    // 더하기 하면 X, Y 모두 조절 가능! 아래는 메뉴얼
+    // https://docs.unity3d.com/kr/530/ScriptReference/Vector3.html
+    void Update()
+    {
+        this.cube_red.transform.SetPositionAndRotation(Vector3.right * 5 + Vector3.forward * 5, Quaternion.identity);
+    }
+    */
 }
