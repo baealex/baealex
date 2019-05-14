53aa5864d8f4d495809729cad462c9d45955754c
Jino Bae <im@baejino.com>
2019-05-15 00:38:12 +0900

    Update DinamicCreateObject.cs

--- Unity/DinamicCreateObject.cs
+++ Unity/DinamicCreateObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class DinamicCreateObject : MonoBehaviour
 {
     GameObject cube_red;
     GameObject cube_blue;
