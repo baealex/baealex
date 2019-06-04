87d29bc5a77d679782240bfbbb38dccd23d85c96
baealex <im@baejino.com>
2019-06-05 01:07:00 +0900

    remove tensorflow

--- HTML/Canvas-Template/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.getElementById("myCanvas");
-var cts = canvas.getContext("2d");
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-setInterval(draw, 10);
--- HTML/HTML_Class_Tutorial_2/js/model.js
+++ HTML/HTML_Class_Tutorial_2/js/model.js
@@ -37,94 +37,33 @@ var Player = function(color) {
         }
         // */
 
+        // /*
+
         // 가속도 운동
 
         if(moveKeyState[0] == 1) {
-            this.sx -= 0.5;
-            if(this.sx < -20) {
-                this.sx = -20;
-            }
-            this.x += this.sx;
-            if(this.x < 0) {
-                this.x = 0;
-                this.sx = 0;
-            }
-        } else {
-            if(this.sx < 0) {
-                this.sx += 1;
-                if(this.sx >= 0) {
-                    this.sx = 0;
-                }
-            }
         }
 
         if(moveKeyState[1] == 1) {
-            this.sy -= 0.5;
-            if(this.sy < -20) {
-                this.sy = -20;
-            }
-            this.y += this.sy;
-            if(this.y < 0) {
-                this.y = 0;
-                this.sy = 0;
-            }
-        } else {
-            if(this.sy < 0) {
-                this.sy += 1;
-                if(this.sy >= 0) {
-                    this.sy = 0;
-                }
-            }
         }
 
         if(moveKeyState[2] == 1) {
-            this.sx += 0.5;
-            if(this.sx > 20) {
-                this.sx = 20;
-            }
-            this.x += this.sx;
-            if(this.x > canvas.width - this.w) {
-                this.x = canvas.width - this.w;
-                this.sx = 0;
-            }
-        } else {
-            if(this.sx > 0) {
-                this.sx -= 1;
-                if(this.sx <= 0) {
-                    this.sx = 0;
-                }
-            }
         }
 
         if(moveKeyState[3] == 1) {
-            this.sy += 0.5;
-            if(this.sy > 20) {
-                this.sy = 20;
-            }
-            this.y += this.sy;
-            if(this.y > canvas.height - this.h) {
-                this.y = canvas.height - this.h;
-                this.sy = 0;
-            }
-        } else {
-            if(this.sy > 0) {
-                this.sy += 1;
-                if(this.sy <= 0) {
-                    this.sy = 0;
-                }
-            }
         }
+        // */
     };
 
     this.Render = function() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x,this.y,this.w,this.h);
     };
-}
+};
 
 var OtherBall = function() {
     var colors = ['#fff', '#ff0', '#f0f', '#0ff', '#f00', '#0f0', '#00f'];
-    this.color = colors[parseInt((Math.random * 10000) % colors.length)];
+    this.color = colors[parseInt(Math.random() * 10000 % colors.length)];
     this.mx = canvas.width;
     this.my = canvas.height;
     this.x = parseInt((Math.random()* 9000000) % this.mx)
@@ -150,7 +89,6 @@ var OtherBall = function() {
     };
     
     this.Render = function() {
-        console.debug(this.color);
         ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x + (this.w/2), this.y + (this.h/2), this.w, 0, Math.PI * 2, true);
