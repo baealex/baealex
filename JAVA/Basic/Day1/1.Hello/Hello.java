8cbb5edc50029ea4298e2738ffdb151764235eef
Jino Bae <im@baejino.com>
2019-07-10 11:15:43 +0900

    javastudy - basic - day1

---
+++ JAVA/Basic/Day1/1.Hello/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+	static public void main(String [] args) {
+		String a = "Hi~~";
+		System.out.println(a);
+	}
+}
---
+++ JAVA/Basic/Day1/2.Hello/Hello2.java
@@ -0,0 +1,17 @@
+public class Hello2 {
+	public static int sum(int n, int m) {
+		return n+m;
+	}
+	
+	public static void main(String [] args) {
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i, 10);
+		a = '?';
+		System.out.println(a);
+		System.out.println("Hello2");
+		System.out.println(s);
+	}
+}
