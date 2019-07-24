8d25de0e8e8156a02e89d5d81ebb0db30f1e618f
baealex <im@baejino.com>
2019-07-24 19:11:13 +0900

    node - chat - update

--- JavaScript/Node/Basic/Day05/chat/server.js
+++ JavaScript/Node/Basic/Day05/chat/server.js
@@ -7,29 +7,37 @@ app.get('/',function(req, res){
   res.sendFile(__dirname + '/client.html');
 });
 
-var count=1;
+app.get('/main.css',function(req, res){
+  res.sendFile(__dirname + '/main.css');
+});
+
+var tempNick = ["감자탕","맛탕","새우탕",
+				"그라탕","갈비탕","백설탕"];
+
 io.on('connection', function(socket){
-  console.log('user connected: ', socket.id);
-  var name = "user" + count++;
-  io.to(socket.id).emit('change name',name);
+  console.log('User Connected: ', socket.id);
+  var name = tempNick[Math.floor(Math.random()*6)];
+  io.to(socket.id).emit('change name', name);
 
   socket.on('disconnect', function(){
-    console.log('user disconnected: ', socket.id);
+      console.log('User Disconnected: ', socket.id);
   });
 
   socket.on('rename', function(name,text){
-	  console.log(socket.id + ' : ' + name + ' to ' + text);
-      io.to(socket.id).emit('change name',text);
-	  io.emit('receive message', name + ' 님이 ' + text + ' (으)로 닉변했습니다!');
+	  console.log(socket.id + '(' + name + ') => ' + text);
+      io.to(socket.id).emit('change name', text);
+	  io.emit('receive message', name +'님이 '+text+'(으)로 닉네임을 변경했습니다.');
   });
   
-  socket.on('send message', function(name,text){
-    var msg = name + ' : ' + text;
-    console.log(msg);
-    io.emit('receive message', msg);
+  socket.on('send message', function(name, text){
+	  massage = name + ' : ' + text;
+      console.log(socket.id + '(' + name + ') : ' + text);
+      io.emit('receive message', massage);
   });
 });
 
-http.listen(8080, function(){
-  console.log('server on!');
+var port = 8080;
+
+http.listen(port, function(){
+  console.log('SERVER Run in port '+port+'.');
 });
