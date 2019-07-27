008910aea2cd1379412f6f6e11796ba8570dce4f
baealex <im@baejino.com>
2019-07-27 13:56:07 +0900

    jnodejs - chat - style change to es6

--- JavaScript/Node/Basic/Day05/chat/server.js
+++ JavaScript/Node/Basic/Day05/chat/server.js
@@ -2,34 +2,34 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var regex_pattern
-app.get('/',function(req, res){
-  res.sendFile(__dirname + '/client.html');
+
+app.get('/',(req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/main.css',function(req, res){
+app.get('/main.css', (req, res) => {
   res.sendFile(__dirname + '/main.css');
 });
 
 var tempNick = ["감자탕","맛탕","새우탕",
 				"그라탕","갈비탕","백설탕"];
 
-io.on('connection', function(socket){
+io.on('connection', socket => {
   console.log('User Connected: ', socket.id);
   var name = tempNick[Math.floor(Math.random()*6)];
   io.to(socket.id).emit('change name', name);
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', () => {
       console.log('User Disconnected: ', socket.id);
   });
 
-  socket.on('rename', function(name,text){
+  socket.on('rename', (name,text) => {
 	  console.log(socket.id + '(' + name + ') => ' + text);
       io.to(socket.id).emit('change name', text);
 	  io.emit('receive message', name +'님이 '+text+'(으)로 닉네임을 변경했습니다.');
   });
   
-  socket.on('send message', function(name, text){
+  socket.on('send message', (name, text) => {
 	  massage = name + ' : ' + text;
       console.log(socket.id + '(' + name + ') : ' + text);
       io.emit('receive message', massage);
