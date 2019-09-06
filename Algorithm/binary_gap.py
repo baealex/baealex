1150ad50dd12053a980f40a833752cd554aa3ba5
baealex <im@baejino.com>
2019-09-06 21:17:47 +0900

    problem solve

--- Algorithm/binary_gap.py
+++ Algorithm/binary_gap.py
@@ -1,36 +1,31 @@
-def createBinary(N):
+def trans_binary(N):
     result = ''
     while True:
-        if N == 0 or N == 1:
-            result += str(N)
-            break
         result += str(N%2)
         N -= N%2
-        N /= 2
-        N = int(N)
+        N = int(N/2)
+        if(N == 0 or N == 1):
+            result += str(N)
+            break
     return result[::-1]
-    
 
 def solution(N):
-    binary = createBinary(N)
-    print(binary)
-    startPoint = -1
-    endPoint = -1
+    # write your code in Python 3.6
+    firstPoint = -1
+    lastPoint = -1
     maxLength = 0
     tempLength = 0
     
-    for point in range(len(binary)):
-        if binary[point] == '1' and startPoint == -1:
-            startPoint = point
-        elif binary[point] == '1' and not startPoint == -1:
-            endPoint = point
-        if not startPoint == -1 and not endPoint == -1:
-            tempLength = endPoint - startPoint -1
-            if tempLength > maxLength:
-                maxLength = tempLength
-            endPoint = -1
-            startPoint = -1
-    
+    binaryN = trans_binary(N)
+    for i in range(len(binaryN)):
+        if binaryN[i] == '1':
+            if firstPoint == -1:
+                firstPoint = i
+            elif not firstPoint == -1 and lastPoint == -1:
+                lastPoint = i
+                tempLength = lastPoint - firstPoint - 1
+                if tempLength > maxLength:
+                    maxLength = tempLength
+                lastPoint = -1
+                firstPoint = i
     return maxLength
-
-print(solution(1041))
