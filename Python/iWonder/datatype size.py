f14f888d022bec12491ff1bcae42b270ebe58053
baealex <im@baejino.com>
2019-11-09 21:46:26 +0900

    python - i was wondering

---
+++ Python/iWonder/datatype size.py
@@ -0,0 +1,8 @@
+import sys
+
+print('Int size :', sys.getsizeof(1))
+print('Bool size :', sys.getsizeof(True))
+print('Char size :', sys.getsizeof('b'))
+print('String size :', sys.getsizeof('BaeJino'))
+print('Byte size :', sys.getsizeof(b'b'))
+print('Bytes size :', sys.getsizeof(b'BaeJino'))
