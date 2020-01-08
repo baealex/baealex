96928aab53312b3c7fec9ba0b1838b679dc4d988
baealex <im@baejino.com>
2020-01-08 09:46:50 +0900

    update

--- Python/VersionControll/test.py
+++ Python/VersionControll/test.py
@@ -1,12 +1,43 @@
 #-*- coding:utf-8 -*-
 
-def is_change(old_text, new_text):
+def is_changed(old_text, new_text):
     if len(old_text) == len(new_text):
         if len(set(old_text) - set(new_text)) == 0:
             return False
     return True
 
-old_text = ["가다", 0]
-new_text = ["가다", 1]
+def changed_list(old_text, new_text):
+    point = 0
+    data = {
+        'results': list(),
+    }
 
-print(is_change(old_text[0], new_text[0])) # True
+    new_list = {
+        'point': int(),
+        'old': str(),
+        'new': str()
+    }
+    while True:
+        if not old_text[point] == new_text[point]:
+            new_list['point'] = point
+            new_list['old'] += old_text[point]
+            new_list['new'] += new_text[point]
+            break
+        point += 1
+    data['results'].append(new_list)
+    return data
+
+import re
+def remove_common(old_text, new_text):
+    old_word_lists = re.split(' |\n', old_text)
+    new_word_lists = re.split(' |\n', new_text)
+    return {
+        'old': set(old_word_lists) - set(new_word_lists),
+        'new': set(new_word_lists) - set(old_word_lists), 
+    }
+
+for i in remove_common(old_text[0], new_text[0])['new']:
+    print(i)
+print('-----')
+for i in remove_common(old_text[0], new_text[0])['old']:
+    print(i)
