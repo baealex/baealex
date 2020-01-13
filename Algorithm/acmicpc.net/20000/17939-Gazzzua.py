b896eae2640d0562ca0d170b3e58706c05ba31f1
baealex <im@baejino.com>
2020-01-13 21:57:47 +0900

    solve 17939

--- Algorithm/acmicpc.net/20000/17939-Gazzzua.py
+++ Algorithm/acmicpc.net/20000/17939-Gazzzua.py
@@ -1,24 +1,33 @@
 
-# STATE : NOT YET
+# STATE : DONE
+
+def max_dict(lists):
+    index = 0
+    value = 0
+
+    for i in range(len(lists)):
+        if value < lists[i]:
+            value = lists[i]
+            index = i
+
+    return {
+        'value': value,
+        'index': index,
+    }
 
 if __name__ == '__main__':
     N = int(input())
     price_lists = list(map(int, input().split()))
-    
-    item_lists = []
-    last_price = 0
     benefit = 0
+    index = 0
 
-    for i in range(N - 1):
-        if not last_price < price_lists[i+1]:
-            if item_lists:
-                for i in range(item_lists):
-                    benefit += (last_price - item_lists[i])
-                    item_lists.remove(i)
+    while True:
+        max_price = max_dict(price_lists)
+        for i in range(max_price['index']):
+            benefit += (max_price['value'] - price_lists[i])
 
-        if price_lists[i] < price_lists[i+1]:
-            item_lists.append(price_lists[i])
-            
+        price_lists = price_lists[max_price['index'] + 1:]
+        if len(price_lists) == 0:
+            break
 
-        last_price = price_lists[i]
     print(benefit)
