2b338d255f0700d672f0ce99df57b24e1c902d89
baealex <im@baejino.com>
2020-01-17 17:37:02 +0900

    solve 1463

---
+++ Algorithm/acmicpc.net/5000/1463-1로-만들기.py
@@ -0,0 +1,16 @@
+
+# URL   : https://www.acmicpc.net/problem/1463
+# STATE : DONE(2020-01-17)
+
+min = lambda x, y : x < y and x or y
+
+if __name__=='__main__':
+    N = int(input())
+
+    DP = [0 for _ in range((N + 1) * 3)]
+    for i in range(1, N+1):
+        DP[i + 1] = min(DP[i + 1], DP[i] + 1)
+        DP[i * 2] = min(DP[i * 2], DP[i] + 1)
+        DP[i * 3] = min(DP[i * 3], DP[i] + 1)
+    
+    print(DP[N])
