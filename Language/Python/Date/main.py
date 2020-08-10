4dd24e406eebcfa28755466fb0f41d3a8987063a
baealex <im@baejino.com>
2020-08-10 20:21:00 +0900

    date util

---
+++ Language/Python/Date/main.py
@@ -0,0 +1,38 @@
+def get_month_day(year, month):
+    if month == 4 or month == 6 or month == 9 or month == 11:
+        return 30
+    elif month == 2:
+        if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+            return 29
+        else:
+            return 28
+    else:
+        return 31
+
+def get_total_day(year, month, day):
+    year_days = (year-1)*365 + ((int((year-1)/4) - int((year-1)/100)) + int((year-1)/400))
+
+    month_days = 0
+    for i in range(1, month):
+        month_days += get_month_day(year, i)
+
+    return year_days + month_days + day
+
+def get_weekday(day):
+    day %= 7
+    if day == 0:
+        return 'SUN'
+    if day == 1:
+        return 'MON'
+    if day == 2:
+        return 'TUE'
+    if day == 3:
+        return 'WED'
+    if day == 4:
+        return 'THU'
+    if day == 5:
+        return 'FRI'
+    if day == 6:
+        return 'SAT'
+
+print(get_weekday(get_total_day(2020, 1, 30)))
