d98066447bda7dcb0e966d66c4ece4b482d625bf
baealex <im@baejino.com>
2020-08-10 20:21:42 +0900

    머야 이거 왜 안돼

---
+++ Language/JavaScript/solar-to-lunar/lib/Solar.js
@@ -0,0 +1,7 @@
+module.exports = class Solar {
+    constructor(solarYear=0, solarMonth=0, solarDay=0) {
+        this.solarDay = solarDay;
+        this.solarMonth = solarMonth;
+        this.solarYear = solarYear;
+    }
+}
