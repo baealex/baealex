621a3856ce33583f5207e7daf088112ba7304be0
baealex <im@baejino.com>
2020-09-09 10:46:43 +0900

    move dir

--- Language/JavaScript/solar-to-lunar/lib/Lunar.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Lunar {
-    constructor(lunarYear=0, lunarMonth=0, lunarDay=0, isleap=false) {
-        this.isleap = isleap;
-        this.lunarDay = lunarDay;
-        this.lunarMonth = lunarMonth;
-        this.lunarYear = lunarYear;
-    }
-}
--- Language/JavaScript/solar-to-lunar/lib/Solar.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Solar {
-    constructor(solarYear=0, solarMonth=0, solarDay=0) {
-        this.solarDay = solarDay;
-        this.solarMonth = solarMonth;
-        this.solarYear = solarYear;
-    }
-}
