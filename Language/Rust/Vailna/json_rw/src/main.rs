9aa8828626c881d976b6cd036ba5e0a7f47206aa
baealex <im@baejino.com>
2021-03-03 18:07:24 +0900

    rust json rw

---
+++ Language/Rust/Vailna/json_rw/src/main.rs
@@ -0,0 +1,14 @@
+extern crate rustc_serialize;
+use rustc_serialize::json::Json;
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let mut file = File::open("text.json").unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+
+    let json = Json::from_str(&data).unwrap();
+    // println!("{}", json[0]["aa"]);
+    file.write_all(&json);
+}
