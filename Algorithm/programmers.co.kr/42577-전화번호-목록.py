13802c1a2cbd19c290cde4417c5984cac15d9179
Jino Bae <im@baejino.com>
2021-10-25 21:48:29 +0900

    solve 42577 in programmers

---
+++ Algorithm/programmers.co.kr/42577-전화번호-목록.py
@@ -0,0 +1,15 @@
+def solution(phone_book):
+    book_map = {}
+
+    for phone in phone_book:
+        book_map[phone] = True
+
+    for phone in phone_book:
+        for length in range(1, len(phone)):
+            try:
+                if book_map[phone[0:length]]:
+                    return False
+            except:
+                pass
+
+    return True
