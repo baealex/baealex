108baf18ae6cfaae690af0928cd15658fad862b1
Jino Bae <im@baejino.com>
2021-12-30 16:48:33 +0900

    fix javascript deepcopy issue

--- languages/JavaScript/deepcopy.ts
+++ languages/JavaScript/deepcopy.ts
@@ -2,11 +2,6 @@ function getTypes(prop: any) {
     return Object.getPrototypeOf(prop).constructor.name
 }
 
-function getFuncName(func: Function) {
-    const temp = func.toString().substr('function '.length);
-    return temp.substr(0, temp.indexOf('('));
-}
-
 function deepCopy1(props: any): Object {
     if (typeof props !== 'object') {
         return props;
@@ -14,10 +9,18 @@ function deepCopy1(props: any): Object {
 
     return Object.keys(props).reduce((acc, key) => {
         const curProp = props[key];
-        if (typeof curProp === 'object') {
-            const copiedProp = deepCopy1(curProp);
-            return {...acc, [key]: copiedProp };
+     
+        if (typeof props === 'object') {
+            if (getTypes(curProp) === 'Array') {
+                return {...acc, [key]: [...curProp] };
+            }
+            
+            if (getTypes(curProp) === 'Object') {
+                const copiedProp = deepCopy1(curProp);
+                return {...acc, [key]: copiedProp };
+            }
         }
+
         return {...acc, [key]: curProp };
     }, {})
 }
@@ -26,8 +29,8 @@ function deepCopy2(props: any): Object {
     return JSON.parse(JSON.stringify(props))
 }
 
-function test(copyFunc: Function) {
-    console.log(`===== ${getFuncName(copyFunc)} =====`);
+function test(label: string, copyFunc: Function) {
+    console.log(`===== ${label} =====`);
     
     let x = {
         y: {
@@ -43,22 +46,15 @@ function test(copyFunc: Function) {
     
     console.log(`before change x : ${JSON.stringify(x)}`)
     console.log(`before change y : ${JSON.stringify(y)}`)
-
-    // issue1 : y.y.q.p type is Object on deepCopy1
-    console.log(`before change x.y.q.p type : ${getTypes(x.y.q.p)}`)
-    console.log(`before change y.y.q.p type : ${getTypes(y.y.q.p)}`)
     
     y.y.t = '222'
     y.y.q.p = '456'.split('')
     
     console.log(`after change x : ${JSON.stringify(x)}`)
     console.log(`after change y : ${JSON.stringify(y)}`)
-    
-    console.log(`after change x.y.q.p : ${JSON.stringify(x.y.q.p)}`)
-    console.log(`after change y.y.q.p : ${JSON.stringify(y.y.q.p)}`)
 }
 
 (() => {
-    test(deepCopy1)
-    test(deepCopy2)
+    test('deepCopy1', deepCopy1)
+    test('deepCopy2', deepCopy2)
 })()
