8235bf7ba62bfe9e6086a8dd6b57611e09a9e5a2
Jino Bae <im@baejino.com>
2022-03-10 21:17:48 +0900

    improve vdom lib

--- tiny-projects/my-vdom/assets/scripts/lib/virtual-dom.js
+++ tiny-projects/my-vdom/assets/scripts/lib/virtual-dom.js
@@ -1,5 +1,9 @@
 export function createVirtualDOM(type, props, ...children) {
-    return { type, props, children };
+    return {
+        type,
+        props,
+        children: children.filter(i => !!i),
+    };
 }
 
 function changed(node1, node2) {
@@ -7,7 +11,9 @@ function changed(node1, node2) {
         typeof node1 !== typeof node2 ||
         typeof node1 === 'string' && node1 !== node2 ||
         node1.type !== node2.type ||
-        node1.props?.value === '' && node2.props?.value !== '' ||
+        node1.props?.value !== node2.props?.value ||
+        node1.props?.onClick !== node2.props?.onClick ||
+        node1.props?.onChange !== node2.props?.onChange ||
         node1.props?.className !== node2.props?.className
     );
 }
@@ -21,9 +27,13 @@ function createElement(node) {
     node.children
         .map(createElement)
         .forEach($el.appendChild.bind($el));
-
+    
     if (node.props.className) {
         $el.className = node.props.className;
+    }
+
+    if (node.props.value) {
+        $el.value = node.props.value;
     }
 
     if (node.props.onClick) {
@@ -31,23 +41,28 @@ function createElement(node) {
     }
 
     if (node.props.onChange) {
-        $el.addEventListener('input', node.props.onChange);
+        $el.addEventListener('change', node.props.onChange);
     }
 
     return $el;
 }
 
+let correctionValue = 0;
+
 export function updateElement($parent, newNode, oldNode, index = 0) {
     if (!oldNode) {
+        correctionValue = 0;
         $parent.appendChild(createElement(newNode));
     } else if (!newNode) {
-        $parent.removeChild($parent.childNodes[index]);
+        $parent.removeChild($parent.childNodes[index - correctionValue++]);
     } else if (changed(newNode, oldNode)) {
+        correctionValue = 0;
         $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
     } else if (newNode.type) {
+        correctionValue = 0;
         const newLength = newNode.children.length;
         const oldLength = oldNode.children.length;
-
+ 
         for (let i = 0; i < newLength || i < oldLength; i++) {
             updateElement(
                 $parent.childNodes[index],
